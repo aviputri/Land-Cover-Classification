d7fc08026e653c90796090720ec2b3c24ae70198
Avi Putri Pertiwi <aviputripertiwi@Bernstein-Rundkopf.local>
2018-12-04 16:53:24 +0100

    Add existing file

--- write_raster_11_from09.py
+++ write_raster_11_from09.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #load result
-a = "/Volumes/ga87rif/Study Project/Result/predict_11_50trees_from11full.npy"
+a = "/Volumes/ga87rif/Study Project/Result/predict_11_50trees_from09full.npy"
 predict2011 = np.load(a)
 
 #reshape into 2D
@@ -16,7 +16,7 @@ from rasterio.transform import from_origin
 #transform = from_origin(110....<--dari origin, -7....<--bukan dari origin, 0.000271658, -0.000271658)
 transform = from_origin(110.0363020639564269, -7.8701315292535803, 0.000271658, -0.000271658)
 
-new_dataset = rasterio.open('/Volumes/ga87rif/Study Project/Result/image_11_50trees_from11full.tif', 'w', driver='GTiff',
+new_dataset = rasterio.open('/Volumes/ga87rif/Study Project/Result/image_11_50trees_from09full.tif', 'w', driver='GTiff',
                             height = c.shape[0], width = c.shape[1],
                             count=1, dtype=c.dtype,
                             crs='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',
