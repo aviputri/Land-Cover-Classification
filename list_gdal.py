e539f35b52aec2ba2ccb33e820d8f77ec3387f07
Avi Putri Pertiwi <aviputripertiwi@Bernstein-Rundkopf.local>
2018-12-04 11:28:59 +0100

    -a

---
+++ list_gdal.py
@@ -0,0 +1,2 @@
+import sys;
+sys.path.insert(0,'/Library/Frameworks/GDAL.framework/Versions/2.1/Python/2.7/site-packages');
--- write_raster_rasterio.py
+++ write_raster_rasterio.py
@@ -15,7 +15,7 @@ from rasterio.transform import from_origin
 #transform = from_origin(110....<--dari origin, -7....<--bukan dari origin, 0.000271658, -0.000271658)
 transform = from_origin(110.0363020639564269, -7.8701315292535803, 0.000271658, -0.000271658)
 
-new_dataset = rasterio.open('/Volumes/ga87rif/Study Project/Result/test8.tif', 'w', driver='GTiff',
+new_dataset = rasterio.open('/Volumes/ga87rif/Study Project/Result/image_09_50trees_cropped.tif', 'w', driver='GTiff',
                             height = b.shape[0], width = c.shape[1],
                             count=1, dtype=c.dtype,
                             crs='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',
