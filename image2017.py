b4378530d6f4db96fb44484a4207364b9b38fadf
Avi Putri Pertiwi <aviputripertiwi@Bernstein-Rundkopf.local>
2018-12-07 22:34:10 +0100

    Add existing file

--- image2017.py
+++ image2017.py
@@ -9,30 +9,37 @@ gdal.AllRegister()
 
 b2_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b2_r.tif', gdal.GA_ReadOnly)
 b2_17 = b2_17_ds.GetRasterBand(1).ReadAsArray()
+b2_17 = b2_17[20:9042,20:9187]
 b2_17_r = b2_17.ravel()
 
 b3_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b3_r.tif', gdal.GA_ReadOnly)
 b3_17 = b3_17_ds.GetRasterBand(1).ReadAsArray()
+b3_17 = b3_17[20:9042,20:9187]
 b3_17_r = b3_17.ravel()
 
 b4_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b4_r.tif', gdal.GA_ReadOnly)
 b4_17 = b4_17_ds.GetRasterBand(1).ReadAsArray()
+b4_17 = b4_17[20:9042,20:9187]
 b4_17_r = b4_17.ravel()
 
 b8_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b8_r.tif', gdal.GA_ReadOnly)
 b8_17 = b8_17_ds.GetRasterBand(1).ReadAsArray()
+b8_17 = b8_17[20:9042,20:9187]
 b8_17_r = b8_17.ravel()
 
 b11_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b11_r.tif', gdal.GA_ReadOnly)
 b11_17 = b11_17_ds.GetRasterBand(1).ReadAsArray()
+b11_17 = b11_17[20:9042,20:9187]
 b11_17_r = b11_17.ravel()
 
 b12_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/clip_b12_r.tif', gdal.GA_ReadOnly)
 b12_17 = b12_17_ds.GetRasterBand(1).ReadAsArray()
+b12_17 = b12_17[20:9042,20:9187]
 b12_17_r = b12_17.ravel()
 
 ndvi_17_ds = gdal.Open('/Volumes/ga87rif/Study Project/satelite images/2017/Sentinel 2/ndvi_r.tif', gdal.GA_ReadOnly)
 ndvi_17 = ndvi_17_ds.GetRasterBand(1).ReadAsArray()
+ndvi_17 = ndvi_17[20:9042,20:9187]
 ndvi_17_r = ndvi_17.ravel()
 
 #stack these bands
